5558df43b3ab08f1f20482da5cfb1be55497eb07
proy05 <79052853+proy05@users.noreply.github.com>
2026-07-01 21:22:17 +0530

    demo angular components for home (parent) component and greeting (child) component, and passing data from parent to child

---
+++ src/app/components/greeting/greeting.component.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-greeting',
+  standalone: true,
+  imports: [],
+  templateUrl: './greeting.component.html',
+  styleUrl: './greeting.component.scss'
+})
+export class GreetingComponent {
+  message = input("Hello hello"); //input tells Ng that this component can receive something from the outside
+
+}
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  title = signal("My First Angular App");
+  // title = "My First Angular App"; // without signal (traditional way)
+
+}

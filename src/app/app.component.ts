03ac69d14509df475ec1bab915be1caba2eb4e2a
proy05 <79052853+proy05@users.noreply.github.com>
2026-07-12 19:55:03 +0530

    add routeroutlet for Home and Todos and apply css to corr. nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+// import { HomeComponent } from './home/home.component'; /this import for HomeComponent is not needed in AppComponent, as its already imported in app.routes.ts
+//Basically we will render the HomeComponent and TodoComponent using the RouterOutlet instead of AppComponent
 import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  imports: [RouterOutlet, 
+    // HomeComponent,
+    HeaderComponent],
   template: `
     <app-header />
     <main>
-      <app-home />
+      <!-- <app-home /> -->
+       <router-outlet />
     </main>  
   `,
   styles: [`
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', //root path
+        pathMatch: 'full', //required if path is empty route like above
+        loadComponent: () => {
+            return import('./home/home.component').then(module => module.HomeComponent) //return HomeComponent class
+        }
+
+    },
+    {
+        path: 'todos',
+        loadComponent: () => {
+            return import('./todos/todos.component').then(module => module.TodosComponent)
+        }
+
+    }
+];
